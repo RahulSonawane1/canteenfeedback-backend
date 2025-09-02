71b7d54d24838d1385116013c8a71c9bcdfaaeed
RahulSonawane1 <rahulsonaw2001@gmail.com>
2025-09-02 16:15:02 +0530

    Replaced bcrypt with bcryptjs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import mysql from 'mysql2/promise';
 import { generateToken, verifyToken } from './jwt.js';
 import sitesRouter from './sites.js';
